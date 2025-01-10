6954f63ab9a344135795a033846ff5efb686adca
kt-eluo <kt.eluo@eluocnc.com>
2025-01-10 13:01:34 +0900

    250110-01

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,190 +1,133 @@
-/*
-	Hyperspace by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function($) {
 
 	var	$window = $(window),
 		$body = $('body'),
 		$sidebar = $('#sidebar');
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:   [ '1281px',  '1680px' ],
-			large:    [ '981px',   '1280px' ],
-			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ null,      '480px'  ]
-		});
+	// 브레이크포인트
+	breakpoints({
+		xlarge:   [ '1281px',  '1680px' ],
+		large:    [ '981px',   '1280px' ],
+		medium:   [ '737px',   '980px'  ],
+		small:    [ '481px',   '736px'  ],
+		xsmall:   [ null,      '480px'  ]
+	});
 
-	// Hack: Enable IE flexbox workarounds.
-		if (browser.name == 'ie')
-			$body.addClass('is-ie');
+	// 핵: IE 플렉스박스 워크어라운드 활성화
+	if (browser.name == 'ie')
+		$body.addClass('is-ie');
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+	// 페이지 로드 시 초기 애니메이션 실행
+	$window.on('load', function() {
+		window.setTimeout(function() {
+			$body.removeClass('is-preload');
+		}, 100);
+	});
 
-	// Forms.
 
-		// Hack: Activate non-input submits.
-			$('form').on('click', '.submit', function(event) {
 
-				// Stop propagation, default.
-					event.stopPropagation();
-					event.preventDefault();
+	// 사이드바
+	if ($sidebar.length > 0) {
+		var $sidebar_a = $sidebar.find('a');
 
-				// Submit form.
-					$(this).parents('form').submit();
+		$sidebar_a
+			.addClass('scrolly')
+			.on('click', function() {
+				var $this = $(this);
 
-			});
+				// 외부 링크 중단
+				if ($this.attr('href').charAt(0) != '#')
+					return;
 
-	// Sidebar.
-		if ($sidebar.length > 0) {
+				// 모든 링크 비활성화
+				$sidebar_a.removeClass('active');
 
-			var $sidebar_a = $sidebar.find('a');
-
-			$sidebar_a
-				.addClass('scrolly')
-				.on('click', function() {
-
-					var $this = $(this);
-
-					// External link? Bail.
-						if ($this.attr('href').charAt(0) != '#')
-							return;
-
-					// Deactivate all links.
-						$sidebar_a.removeClass('active');
-
-					// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
-						$this
-							.addClass('active')
-							.addClass('active-locked');
-
-				})
-				.each(function() {
-
-					var	$this = $(this),
-						id = $this.attr('href'),
-						$section = $(id);
-
-					// No section for this link? Bail.
-						if ($section.length < 1)
-							return;
-
-					// Scrollex.
-						$section.scrollex({
-							mode: 'middle',
-							top: '-20vh',
-							bottom: '-20vh',
-							initialize: function() {
-
-								// Deactivate section.
-									$section.addClass('inactive');
-
-							},
-							enter: function() {
-
-								// Activate section.
-									$section.removeClass('inactive');
-
-								// No locked links? Deactivate all links and activate this section's one.
-									if ($sidebar_a.filter('.active-locked').length == 0) {
-
-										$sidebar_a.removeClass('active');
-										$this.addClass('active');
-
-									}
-
-								// Otherwise, if this section's link is the one that's locked, unlock it.
-									else if ($this.hasClass('active-locked'))
-										$this.removeClass('active-locked');
-
-							}
-						});
-
-				});
-
-		}
-
-	// Scrolly.
-		$('.scrolly').scrolly({
-			speed: 1000,
-			offset: function() {
-
-				// If <=large, >small, and sidebar is present, use its height as the offset.
-					if (breakpoints.active('<=large')
-					&&	!breakpoints.active('<=small')
-					&&	$sidebar.length > 0)
-						return $sidebar.height();
-
-				return 0;
-
-			}
-		});
-
-	// Spotlights.
-		$('.spotlights > section')
-			.scrollex({
-				mode: 'middle',
-				top: '-10vh',
-				bottom: '-10vh',
-				initialize: function() {
-
-					// Deactivate section.
-						$(this).addClass('inactive');
-
-				},
-				enter: function() {
-
-					// Activate section.
-						$(this).removeClass('inactive');
-
-				}
+				// 링크 활성화 *및* 잠금 (Scrollex가 이 섹션으로 스크롤하는 동안 다른 링크를 활성화하지 않도록)
+				$this
+					.addClass('active')
+					.addClass('active-locked');
 			})
 			.each(function() {
-
 				var	$this = $(this),
-					$image = $this.find('.image'),
-					$img = $image.find('img'),
-					x;
+					id = $this.attr('href'),
+					$section = $(id);
 
-				// Assign image.
-					$image.css('background-image', 'url(' + $img.attr('src') + ')');
+				// 이 링크에 해당하는 섹션이 없을 때 중단
+				if ($section.length < 1)
+					return;
 
-				// Set background position.
-					if (x = $img.data('position'))
-						$image.css('background-position', x);
+				// Scrollex
+				$section.scrollex({
+					mode: 'middle',
+					top: '-20vh',
+					bottom: '-20vh',
+					initialize: function() {
+						// 섹션 비활성화
+						$section.addClass('inactive');
+					},
+					enter: function() {
+						// 섹션 활성화
+						$section.removeClass('inactive');
 
-				// Hide <img>.
-					$img.hide();
-
+						// 링크 없을 시 모든 링크를 비활성화하고 이 섹션의 링크를 활성화
+						if ($sidebar_a.filter('.active-locked').length == 0) {
+							$sidebar_a.removeClass('active');
+							$this.addClass('active');
+						}
+						// 이 섹션의 링크가 잠겨 있다면 잠금 해제
+						else if ($this.hasClass('active-locked'))
+							$this.removeClass('active-locked');
+					}
+				});
 			});
+	}
 
-	// Features.
-		$('.features')
-			.scrollex({
-				mode: 'middle',
-				top: '-20vh',
-				bottom: '-20vh',
-				initialize: function() {
 
-					// Deactivate section.
-						$(this).addClass('inactive');
+	// 스포트라이트
+	$('.spotlights > section')
+		.scrollex({
+			mode: 'middle',
+			top: '-10vh',
+			bottom: '-10vh',
+			initialize: function() {
+				// 섹션 비활성화
+				$(this).addClass('inactive');
+			},
+			enter: function() {
+				// 섹션 활성화
+				$(this).removeClass('inactive');
+			}
+		})
+		.each(function() {
+			var	$this = $(this),
+				$image = $this.find('.image'),
+				$img = $image.find('img'),
+				x;
 
-				},
-				enter: function() {
+			// 이미지 할당
+			$image.css('background-image', 'url(' + $img.attr('src') + ')');
 
-					// Activate section.
-						$(this).removeClass('inactive');
+			// 배경 위치 설정
+			if (x = $img.data('position'))
+				$image.css('background-position', x);
 
-				}
-			});
+			$img.hide();
+		});
+
+	// 특징
+	$('.features')
+		.scrollex({
+			mode: 'middle',
+			top: '-20vh',
+			bottom: '-20vh',
+			initialize: function() {
+				// 섹션 비활성화
+				$(this).addClass('inactive');
+			},
+			enter: function() {
+				// 섹션 활성화
+				$(this).removeClass('inactive');
+			}
+		});
 
 })(jQuery);
