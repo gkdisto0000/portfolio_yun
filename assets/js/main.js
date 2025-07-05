5ec19a63b2e40e76fdc4b96232857cc0079d6ed0
gkdisto0000 <qufqlcto@naver.com>
2025-07-05 22:39:19 +0900

    포트폴리오 전체적으로 수정

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,34 +28,6 @@
 	if ($sidebar.length > 0) {
 		var $sidebar_a = $sidebar.find('a');
 
-		// 스크롤 이벤트 핸들러
-		// `$(window).on('scroll', function() {
-		// 	var currentScroll = $(window).scrollTop() + (window.innerHeight / 2);
-		// 	var introSection = $('#intro');
-		// 	var oneSection = $('#one');
-		// 	var twoSection = $('#two');
-		// 	var threeSection = $('#three');
-		// 	var fourSection = $('#four');
-			
-		// 	if ((oneSection.length && currentScroll >= oneSection.offset().top && currentScroll <= oneSection.offset().top + oneSection.outerHeight()) ||
-		// 		(threeSection.length && currentScroll >= threeSection.offset().top && currentScroll <= threeSection.offset().top + threeSection.outerHeight())) {
-		// 		$('#sidebar nav a').css('color', '#999').removeClass('active');
-		// 		var currentSection = currentScroll >= threeSection.offset().top ? '#three' : '#one';
-		// 		$('#sidebar nav a[href="' + currentSection + '"]').addClass('active').css('color', '#000');
-		// 	} else if (twoSection.length && currentScroll >= twoSection.offset().top && currentScroll <= twoSection.offset().top + twoSection.outerHeight()) {
-		// 		$('#sidebar nav a').css('color', '').removeClass('active');
-		// 		$('#sidebar nav a[href="#two"]').addClass('active');
-		// 	} else if (fourSection.length && currentScroll >= fourSection.offset().top && currentScroll <= fourSection.offset().top + fourSection.outerHeight()) {
-		// 		$('#sidebar nav a').css('color', '').removeClass('active');
-		// 		$('#sidebar nav a[href="#four"]').addClass('active');
-		// 	} else if (introSection.length && currentScroll >= introSection.offset().top && currentScroll <= introSection.offset().top + introSection.outerHeight()) {
-		// 		$('#sidebar nav a').css('color', '').removeClass('active');
-		// 		$('#sidebar nav a[href="#intro"]').addClass('active');
-		// 	} else {
-		// 		$('#sidebar nav a').css('color', '').removeClass('active');
-		// 	}
-		// });`
-
 		$sidebar_a
 			.addClass('scrolly')
 			.on('click', function() {
@@ -149,43 +121,5 @@
 				$(this).removeClass('inactive');
 			}
 		});
-
-	// one, three 섹션에서 사이드바 색상 변경
-	$('section').each(function() {
-		var section = $(this);
-		var sectionId = '#' + section.attr('id');
-		
-		section.scrollex({
-			mode: 'middle',
-			top: '-30%',
-			bottom: '-30%',
-			enter: function() {
-				if (sectionId === '#one' || sectionId === '#three') {
-					$('#sidebar nav a').css('color', '#ddd');
-					$('#sidebar nav a[href="' + sectionId + '"]').css('color', '#000');
-				} else {
-					$('#sidebar nav a').css('color', '');
-				}
-			},
-			leave: function(direction) {
-				// 현재 보이는 섹션 찾기
-				var currentSection = null;
-				$('section').each(function() {
-					var rect = this.getBoundingClientRect();
-					if (rect.top <= window.innerHeight/2 && rect.bottom >= window.innerHeight/2) {
-						currentSection = '#' + $(this).attr('id');
-					}
-				});
-				
-				// 현재 보이는 섹션이 one이나 three면 해당 색상 적용
-				if (currentSection === '#one' || currentSection === '#three') {
-					$('#sidebar nav a').css('color', '#ddd');
-					$('#sidebar nav a[href="' + currentSection + '"]').css('color', '#000');
-				} else {
-					$('#sidebar nav a').css('color', '');
-				}
-			}
-		});
-	});
 
 })(jQuery);
